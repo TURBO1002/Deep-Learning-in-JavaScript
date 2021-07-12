9c4569eb9b39058b5ba942a7f48a17bcda992f6e
石璞东 <41555864+TURBO1002@users.noreply.github.com>
2021-07-12 09:26:09 +0800

    Add files via upload

---
+++ hahaSearchWords/eventPage.js
@@ -0,0 +1,27 @@
+var menuItem = {
+    "id":"hahaWord",
+    "title":"金山词霸：%s",
+    "contexts":["selection"]
+}
+
+chrome.contextMenus.create(menuItem)
+
+function fixedEncodeURI(str){
+    return encodeURI(str).replace(/%5B/g,'[').replace(/%5D/g,']')
+}
+
+chrome.contextMenus.onClicked.addListener(function (clickData){
+    console.log("clickData",clickData)
+    if(clickData.menuItemId =="hahaWord" && clickData.selectionText){
+        var icibaUrl = " http://www.iciba.com/word?w="+clickData.selectionText
+            var createData = {
+            "url":icibaUrl,
+            "type":"popup",
+            "top":50,
+            "left":50,
+            "width":screen.width/2,
+            "height":screen.height/2
+        }
+        chrome.windows.create(createData,function (){})
+    }
+})
